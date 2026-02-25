90cb36508beba33a95c64d86be6bf2ee3f8f74bb
dat-nix <dp2838400@gmail.com>
2026-02-25 22:56:13 +0700

    update bar font

--- config.def.h
+++ config.def.h
@@ -12,10 +12,10 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[] = {
-    "monospace:size=10",
-    "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"
+    "monospace:size=11",
+    "NotoColorEmoji:pixelsize=11:antialias=true:autohint=true"
 };
-static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
