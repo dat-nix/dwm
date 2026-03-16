88680a421ecd41e0bbe404d93ae456ed7cf619f4
dat-nix <dp2838400@gmail.com>
2026-03-17 01:49:23 +0700

    reduce border

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 4; /* border pixel of windows */
+static const unsigned int borderpx = 3; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int scalepreview =
     4; /* preview scaling (display w and h / scalepreview) */
@@ -23,11 +23,11 @@ static const char *fonts[] = {
     "NotoColorEmoji:pixelsize=10.5:antialias=true:autohint=true"};
 static const char dmenufont[] = "monospace:size=11";
 static const char col_gray1[] = "#222222";
-static const char col_gray2[] = "#444444";
+static const char col_gray2[] = "#888888";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
 static const char col_cyan[] = "#005577";
-static const char col_red[] = "#cc241d"; /* focused border */
+static const char col_red[] = "#ff2222"; /* focused border - đỏ thuần */
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
@@ -132,18 +132,6 @@ static const Key keys[] = {
     {MODKEY | Mod1Mask, XK_x, incrgaps, {.i = -3}},
     {MODKEY | Mod1Mask, XK_a, defaultgaps, {0}},
     {MODKEY | Mod1Mask, XK_0, togglegaps, {0}},
-    // { MODKEY|Mod1Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
-    // { MODKEY|Mod1Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
-    // { MODKEY|Mod1Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
-    // { MODKEY|Mod1Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
-    // { MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
-    // { MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
-    // { MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
-    // { MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
-    // { MODKEY|Mod1Mask,              XK_y,      incrohgaps,     {.i = +1 } },
-    // { MODKEY|Mod1Mask,              XK_o,      incrohgaps,     {.i = -1 } },
-    // { MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
-    // { MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
 
     {MODKEY | ShiftMask, XK_Return, togglescratch, {.v = scratchpadcmd}},
     {MODKEY | ShiftMask, XK_l, spawn, SHCMD("loginctl suspend")},
@@ -166,10 +154,6 @@ static const Key keys[] = {
     {MODKEY, XK_0, view, {.ui = ~0}},
     {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
     {MODKEY, XK_F7, spawn, SHCMD("~/.screenlayout/mirror.sh")},
-    // { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-    // { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-    // { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-    // { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
             TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_BackSpace, quit, {0}},
