aa5a3fa257bc987c98fc4a969d1ea274ecb83f11
dat-nix <dp2838400@gmail.com>
2026-03-17 01:57:09 +0700

    add statuspadding patch

--- config.h
+++ config.h
@@ -16,8 +16,10 @@ static const unsigned int gappov =
     30; /* vert outer gap between windows and screen edge */
 static const int smartgaps =
     0; /* 1 means no outer gap when there is only one window */
-static const int showbar = 1; /* 0 means no bar */
-static const int topbar = 1;  /* 0 means bottom bar */
+static const int showbar = 1;     /* 0 means no bar */
+static const int topbar = 1;      /* 0 means bottom bar */
+static const int horizpadbar = 2; /* horizontal padding for statusbar */
+static const int vertpadbar = 0;  /* vertical padding for statusbar */
 static const char *fonts[] = {
     "monospace:size=10.5",
     "NotoColorEmoji:pixelsize=10.5:antialias=true:autohint=true"};
